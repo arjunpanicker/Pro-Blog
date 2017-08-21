3370076743d0e909247c81559dda847d9d1b5c63
Arjun Panicker <arjunpanicker@users.noreply.github.com>
2017-08-21 23:23:26 +0530

    Removed unnecessary comments
    I have removed the unnecessary comments from leftTextInnerSplit.js file.

--- js/leftTextInnerSplit.js
+++ js/leftTextInnerSplit.js
@@ -1,18 +1,18 @@
-var byline = document.getElementById('left-text-inner');  	// Find the H2
-var bylineText = byline.innerHTML;										// Get the content of the H2
-var bylineArr = bylineText.split('');									// Split content into array
-byline.innerHTML = '';														// Empty current content
+var byline = document.getElementById('left-text-inner');  	
+var bylineText = byline.innerHTML;										
+var bylineArr = bylineText.split('');									
+byline.innerHTML = '';														
 
-var span;					// Create variables to create elements
+var span;					
 var letter;
 
-for(let i=0;i<bylineArr.length;i++){									// Loop for every letter
-  span = document.createElement("span");					// Create a <span> element
-  letter = document.createTextNode(bylineArr[i]);	// Create the letter
-  if(bylineArr[i] == ' ') {												// If the letter is a space...
-    byline.appendChild(letter);					// ...Add the space without a span
+for(let i=0;i<bylineArr.length;i++){									
+  span = document.createElement("span");					
+  letter = document.createTextNode(bylineArr[i]);	
+  if(bylineArr[i] == ' ') {												
+    byline.appendChild(letter);					
   } else {
-        span.appendChild(letter);						// Add the letter to the span
-  	byline.appendChild(span); 					// Add the span to the h2
+        span.appendChild(letter);						
+  	byline.appendChild(span); 					
   }
 }
